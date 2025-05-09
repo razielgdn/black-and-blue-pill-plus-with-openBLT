6d37fd6f16821eb68eb49d7e647a0ed64ab033fa
octavioguendulain <Octavio Guendulain>
2025-05-08 21:42:01 -0600

    The project was reorganized and demos were added.

---
+++ openBLTWbluepill/BootCommander/LibOpenBLT/build/CMakeFiles/openblt_shared.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/aes256.c" "CMakeFiles/openblt_shared.dir/aes256.c.o" "gcc" "CMakeFiles/openblt_shared.dir/aes256.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/candriver.c" "CMakeFiles/openblt_shared.dir/candriver.c.o" "gcc" "CMakeFiles/openblt_shared.dir/candriver.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/firmware.c" "CMakeFiles/openblt_shared.dir/firmware.c.o" "gcc" "CMakeFiles/openblt_shared.dir/firmware.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/openblt.c" "CMakeFiles/openblt_shared.dir/openblt.c.o" "gcc" "CMakeFiles/openblt_shared.dir/openblt.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/port/linux/canif/socketcan/socketcan.c" "CMakeFiles/openblt_shared.dir/port/linux/canif/socketcan/socketcan.c.o" "gcc" "CMakeFiles/openblt_shared.dir/port/linux/canif/socketcan/socketcan.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/port/linux/critutil.c" "CMakeFiles/openblt_shared.dir/port/linux/critutil.c.o" "gcc" "CMakeFiles/openblt_shared.dir/port/linux/critutil.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/port/linux/netaccess.c" "CMakeFiles/openblt_shared.dir/port/linux/netaccess.c.o" "gcc" "CMakeFiles/openblt_shared.dir/port/linux/netaccess.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/port/linux/serialport.c" "CMakeFiles/openblt_shared.dir/port/linux/serialport.c.o" "gcc" "CMakeFiles/openblt_shared.dir/port/linux/serialport.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/port/linux/timeutil.c" "CMakeFiles/openblt_shared.dir/port/linux/timeutil.c.o" "gcc" "CMakeFiles/openblt_shared.dir/port/linux/timeutil.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/port/linux/usbbulk.c" "CMakeFiles/openblt_shared.dir/port/linux/usbbulk.c.o" "gcc" "CMakeFiles/openblt_shared.dir/port/linux/usbbulk.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/port/linux/xcpprotect.c" "CMakeFiles/openblt_shared.dir/port/linux/xcpprotect.c.o" "gcc" "CMakeFiles/openblt_shared.dir/port/linux/xcpprotect.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/session.c" "CMakeFiles/openblt_shared.dir/session.c.o" "gcc" "CMakeFiles/openblt_shared.dir/session.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/srecparser.c" "CMakeFiles/openblt_shared.dir/srecparser.c.o" "gcc" "CMakeFiles/openblt_shared.dir/srecparser.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/util.c" "CMakeFiles/openblt_shared.dir/util.c.o" "gcc" "CMakeFiles/openblt_shared.dir/util.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/xcploader.c" "CMakeFiles/openblt_shared.dir/xcploader.c.o" "gcc" "CMakeFiles/openblt_shared.dir/xcploader.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/xcptpcan.c" "CMakeFiles/openblt_shared.dir/xcptpcan.c.o" "gcc" "CMakeFiles/openblt_shared.dir/xcptpcan.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/xcptpmbrtu.c" "CMakeFiles/openblt_shared.dir/xcptpmbrtu.c.o" "gcc" "CMakeFiles/openblt_shared.dir/xcptpmbrtu.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/xcptpnet.c" "CMakeFiles/openblt_shared.dir/xcptpnet.c.o" "gcc" "CMakeFiles/openblt_shared.dir/xcptpnet.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/xcptpuart.c" "CMakeFiles/openblt_shared.dir/xcptpuart.c.o" "gcc" "CMakeFiles/openblt_shared.dir/xcptpuart.c.o.d"
+  "/home/octavioguendulain/github-repos/black-and-blue-pill-plus-with-openBLT/openBLTWbluepill/test-applications/BootCommander/LibOpenBLT/xcptpusb.c" "CMakeFiles/openblt_shared.dir/xcptpusb.c.o" "gcc" "CMakeFiles/openblt_shared.dir/xcptpusb.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
